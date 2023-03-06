65bee49762a585761ced96fb95f1d49bab75112f
Imtious-Coder <iarafat386@gmail.com>
2023-03-06 20:25:49 +0600

    Phase 3

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect } from "react";
-import AnimatedText from "react-animated-text-content";
 import { HiOutlineDownload } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import HeroImage from "../../Assets/Images/hero.png";
@@ -37,52 +36,16 @@ const Index = () => {
                   Imtious Islam
                 </h1>
                 <h4 className="mb-3 text-center text-sm-start">
-                  <AnimatedText
-                    type="words"
-                    animation={{
-                      x: "200px",
-                      y: "-20px",
-                      scale: 1.1,
-                      ease: "ease-in-out",
-                    }}
-                    animationType="float"
-                    interval={0.06}
-                    duration={0.8}
-                    tag="p"
-                    className="animated-paragraph"
-                    includeWhiteSpaces
-                    threshold={0.1}
-                    rootMargin="20%"
-                  >
-                    Professional Bug Finder & React Developer
-                  </AnimatedText>
+                  Professional Bug Finder & React Developer
                 </h4>
                 <h6 className="mb-4">
-                  <AnimatedText
-                    type="throw"
-                    animation={{
-                      x: "200px",
-                      y: "-20px",
-                      scale: 1.1,
-                      ease: "ease-in-out",
-                    }}
-                    animationType="float"
-                    interval={0.02}
-                    duration={0.0}
-                    tag="p"
-                    className="animated-paragraph"
-                    includeWhiteSpaces
-                    threshold={0.1}
-                    rootMargin="20%"
-                  >
-                    2+ years of professional experience as a Frontend Developer.
-                    Passionate and highly skilled in front-end development with
-                    pixel-perfect design and the use of the latest technologies
-                    and tools. Self-motivated, organized, with excellent time
-                    management skills, and willing to take challenges to improve
-                    my knowledge with the success of the organization. Curious
-                    about AI technology.
-                  </AnimatedText>
+                  2+ years of professional experience as a Frontend Developer.
+                  Passionate and highly skilled in front-end development with
+                  pixel-perfect design and the use of the latest technologies
+                  and tools. Self-motivated, organized, with excellent time
+                  management skills, and willing to take challenges to improve
+                  my knowledge with the success of the organization. Curious
+                  about AI technology.
                 </h6>
                 <div className="d-flex justify-content-center justify-content-sm-start">
                   <Link
