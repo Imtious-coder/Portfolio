44cd044871e8bafcff7e13bc4e3d8e8e5784c7fb
Imtious-Coder <iarafat386@gmail.com>
2023-12-05 21:58:19 +0600

    GA4 Added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss';
-import Layout from './Layout/Layout/Layout.jsx';
-import About from './Pages/About/About';
-import AllProjects from './Pages/AllProjects/AllProjects';
-import Home from './Pages/Home/index.jsx';
-import ProjectDetails from './Pages/ProjectDetails/ProjectDetails';
-import Tests from './utils/Tests';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Layout from "./Layout/Layout/Layout.jsx";
+import About from "./Pages/About/About";
+import AllProjects from "./Pages/AllProjects/AllProjects";
+import Home from "./Pages/Home/index.jsx";
+import ProjectDetails from "./Pages/ProjectDetails/ProjectDetails";
+import Tests from "./utils/Tests";
+// // GA4
+import ReactGA from "react-ga4";
 
 function App() {
-  const [hacked, setHacked] = useState(true)
+  const [hacked, setHacked] = useState(true);
 
- useEffect(() => {
-  setTimeout(() => {
-    setHacked(false)
-  }, 3500);
- }, []);
-  
+  useEffect(() => {
+    setTimeout(() => {
+      setHacked(false);
+    }, 3500);
+  }, []);
+
+  // GA4
+  const GA4_ID = "G-FSF4B3VE9V";
+  ReactGA.initialize(GA4_ID);
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+    title: "Portfolio",
+  });
+
   return (
     <>
       <BrowserRouter>
